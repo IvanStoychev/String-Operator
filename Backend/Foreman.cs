a9ba52285102e43f891e7c54c7c552a1dca53ce0
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-03-01 00:00:27 +0200

    Added "Foreman" class to handle requests to the backend and "StringManipulator" class to perform operations on strings.

---
+++ Backend/Foreman.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend
+{
+    public static class Foreman
+    {
+        public static void BuildGithutWiki(string text)
+        {
+            HashSet<string> methodSignaturesSet = StringManipulator.KeepOnlyMethodSignatures(text);
+        }
+    }
+}
---
+++ Backend/StringManipulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Useful.String.Extensions;
+
+namespace Backend
+{
+    internal static class StringManipulator
+    {
+        internal static HashSet<string> KeepOnlyMethodSignatures(string text)
+        {
+            HashSet<string> methodSignaturesSet = new HashSet<string>();
+
+            foreach (string line in text.Split(Environment.NewLine))
+                if (line.Contains("public static void"))
+                    methodSignaturesSet.Add(line);
+
+            return methodSignaturesSet;
+        }
+
+        internal static string BuildGithutWikiFromMethods(HashSet<string> methodSignaturesSet)
+        {
+
+        }
+    }
+}
